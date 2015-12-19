53152d788b4248a4205088a4aca52e3d0a5f2e00
Matt Spaulding <mrmattspaulding@gmail.com>
2015-12-18 23:12:11 -0500

    Cleaning up.

--- AngularMaterialDotNet.App/www/app/filters/reverseFilter.js
+++
@@ -1,7 +0,0 @@
-﻿'use strict';
-app.filter('reverse', function () {
-    return function (items) {
-        if (!items || !items.length) { return; }
-        return items.slice().reverse();
-    };
-});
